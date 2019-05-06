3952af6517e705f5cfc2bc8788289bf51e6c9a99
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-05-06 22:15:13 +0300

    added on demand test

--- consumer_test.go
+++ consumer_test.go
@@ -1,7 +1,9 @@
 package quasar
 
 import (
+	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -43,10 +45,10 @@ func TestConsumer(t *testing.T) {
 		assert.Equal(t, uint64(counter), entry.Sequence)
 		assert.Equal(t, []byte("foo"), entry.Payload)
 
-		if counter == 50 {
-			err = consumer.Ack(entry.Sequence)
-			assert.NoError(t, err)
+		err = consumer.Ack(entry.Sequence)
+		assert.NoError(t, err)
 
+		if counter == 50 {
 			break
 		}
 	}
@@ -66,10 +68,70 @@ func TestConsumer(t *testing.T) {
 		assert.Equal(t, uint64(counter), entry.Sequence)
 		assert.Equal(t, []byte("foo"), entry.Payload)
 
-		if counter == 100 {
-			err = consumer.Ack(entry.Sequence)
-			assert.NoError(t, err)
+		err = consumer.Ack(entry.Sequence)
+		assert.NoError(t, err)
 
+		if counter == 100 {
+			break
+		}
+	}
+
+	consumer.Close()
+	assert.Empty(t, errors)
+
+	position, err := table.Get("foo")
+	assert.NoError(t, err)
+	assert.Equal(t, uint64(100), position)
+
+	err = db.Close()
+	assert.NoError(t, err)
+}
+
+func TestConsumerOnDemand(t *testing.T) {
+	db := openDB(true)
+
+	ledger, err := CreateLedger(db, LedgerOptions{Prefix: "ledger"})
+	assert.NoError(t, err)
+
+	table, err := CreateTable(db, TableOptions{Prefix: "table"})
+	assert.NoError(t, err)
+
+	counter := 0
+	entries := make(chan Entry, 1)
+	errors := make(chan error, 1)
+
+	opts := ConsumerOptions{
+		Name:    "foo",
+		Batch:   10,
+		Entries: entries,
+		Errors:  errors,
+	}
+
+	consumer := NewConsumer(ledger, table, opts)
+
+	go func() {
+		for i := 1; i <= 100; i++ {
+			time.Sleep(time.Duration(rand.Intn(10000)) * time.Microsecond)
+
+			err = ledger.Write(Entry{
+				Sequence: uint64(i),
+				Payload:  []byte("foo"),
+			})
+			assert.NoError(t, err)
+		}
+	}()
+
+	for {
+		counter++
+
+		entry := <-entries
+		assert.Equal(t, uint64(counter), entry.Sequence)
+		assert.Equal(t, []byte("foo"), entry.Payload)
+
+		err = consumer.Ack(entry.Sequence)
+		assert.NoError(t, err)
+
+		if counter == 100 {
 			break
 		}
 	}
