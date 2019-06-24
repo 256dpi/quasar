57ff5b62a3446489d547e49fb6da8c73505fca72
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-06-24 13:25:48 +0300

    added more examples

---
+++ examples/worker/main.go
@@ -0,0 +1,255 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/256dpi/quasar"
+
+	"github.com/montanaflynn/stats"
+)
+
+var wg sync.WaitGroup
+
+var send int64
+var recv int64
+var diffs []float64
+var mutex sync.Mutex
+
+const batch = 1000
+
+func producer(ledger *quasar.Ledger, done <-chan struct{}) {
+	defer wg.Done()
+
+	// create producer
+	producer := quasar.NewProducer(ledger, quasar.ProducerConfig{
+		Batch:   batch,
+		Timeout: time.Millisecond,
+	})
+
+	// ensure closing
+	defer producer.Close()
+
+	// prepare sequence
+	var sequence uint64
+
+	// write entries forever
+	for {
+		// increment sequence
+		sequence++
+
+		// write entry
+		producer.Write(quasar.Entry{
+			Sequence: sequence,
+			Payload:  []byte(time.Now().Format(time.RFC3339Nano)),
+		}, nil)
+
+		// increment
+		mutex.Lock()
+		send += 1
+		mutex.Unlock()
+
+		// limit rate
+		select {
+		case <-time.After(5 * time.Microsecond):
+		case <-done:
+			return
+		}
+	}
+}
+
+func worker(ledger *quasar.Ledger, matrix *quasar.Matrix, done <-chan struct{}) {
+	defer wg.Done()
+
+	// prepare channels
+	entries := make(chan quasar.Entry, batch)
+	errors := make(chan error, 1)
+
+	// create worker
+	worker := quasar.NewWorker(ledger, matrix, quasar.WorkerConfig{
+		Name:    "example",
+		Batch:   batch,
+		Window:  batch * 10,
+		Skip:    batch,
+		Entries: entries,
+		Errors:  errors,
+	})
+
+	// ensure closing
+	defer worker.Close()
+
+	for {
+		// prepare entry
+		var entry quasar.Entry
+
+		// receive entry or error
+		select {
+		case entry = <-entries:
+		case err := <-errors:
+			panic(err)
+		case <-done:
+			return
+		}
+
+		// get timestamp
+		ts, _ := time.Parse(time.RFC3339Nano, string(entry.Payload))
+
+		// calculate diff
+		diff := float64(time.Since(ts)) / float64(time.Millisecond)
+
+		// increment and save diff
+		mutex.Lock()
+		recv++
+		diffs = append(diffs, diff)
+		mutex.Unlock()
+
+		// acknowledge
+		worker.Ack(entry.Sequence)
+	}
+}
+
+func printer(ledger *quasar.Ledger, matrix *quasar.Matrix, done <-chan struct{}) {
+	defer wg.Done()
+
+	// create ticker
+	ticker := time.Tick(time.Second)
+
+	for {
+		// await signal
+		select {
+		case <-ticker:
+		case <-done:
+			return
+		}
+
+		// get data
+		mutex.Lock()
+		r := recv
+		s := send
+		d := diffs
+		recv = 0
+		send = 0
+		diffs = nil
+		mutex.Unlock()
+
+		// get stats
+		min, _ := stats.Min(d)
+		max, _ := stats.Max(d)
+		mean, _ := stats.Mean(d)
+		p90, _ := stats.Percentile(d, 90)
+		p95, _ := stats.Percentile(d, 95)
+		p99, _ := stats.Percentile(d, 99)
+
+		// get tail
+		tail, _, err := matrix.Range()
+		if err != nil {
+			panic(err)
+		}
+
+		// print rate
+		fmt.Printf("send: %d msg/s, ", s)
+		fmt.Printf("recv %d msgs/s, ", r)
+		fmt.Printf("min: %.2fms, ", min)
+		fmt.Printf("mean: %.2fms, ", mean)
+		fmt.Printf("p90: %.2fms, ", p90)
+		fmt.Printf("p95: %.2fms, ", p95)
+		fmt.Printf("p99: %.2fms, ", p99)
+		fmt.Printf("max: %.2fms, ", max)
+		fmt.Printf("length: %d, ", ledger.Length())
+		fmt.Printf("diff: %d\n", ledger.Head()-tail)
+	}
+}
+
+func cleaner(ledger *quasar.Ledger, matrix *quasar.Matrix, done <-chan struct{}) {
+	defer wg.Done()
+
+	// prepare channels
+	errors := make(chan error, 1)
+
+	// create cleaner
+	cleaner := quasar.NewCleaner(ledger, quasar.CleanerConfig{
+		MinRetention: 10000,
+		MaxRetention: 100000,
+		Matrices:     []*quasar.Matrix{matrix},
+		Delay:        100 * time.Millisecond,
+		Errors:       errors,
+	})
+
+	// ensure closing
+	defer cleaner.Close()
+
+	// wait for close
+	select {
+	case err := <-errors:
+		panic(err)
+	case <-done:
+		return
+	}
+}
+
+func main() {
+	// get dir
+	dir, err := filepath.Abs("./data")
+	if err != nil {
+		panic(err)
+	}
+
+	// remove dir
+	err = os.RemoveAll(dir)
+	if err != nil {
+		panic(err)
+	}
+
+	// open db
+	db, err := quasar.OpenDB(dir, quasar.DBConfig{GCInterval: 10 * time.Second})
+	if err != nil {
+		panic(err)
+	}
+
+	// open ledger
+	ledger, err := quasar.CreateLedger(db, quasar.LedgerConfig{
+		Prefix: "ledger",
+		Cache:  batch * 10,
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	// open matrix
+	matrix, err := quasar.CreateMatrix(db, quasar.MatrixConfig{
+		Prefix: "matrix",
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	// create control channel
+	done := make(chan struct{})
+
+	// run routines
+	wg.Add(4)
+	go producer(ledger, done)
+	go worker(ledger, matrix, done)
+	go cleaner(ledger, matrix, done)
+	go printer(ledger, matrix, done)
+
+	// prepare exit
+	exit := make(chan os.Signal, 1)
+	signal.Notify(exit, syscall.SIGINT, syscall.SIGTERM)
+	<-exit
+
+	// close control channel
+	close(done)
+	wg.Wait()
+
+	// close db
+	err = db.Close()
+	if err != nil {
+		panic(err)
+	}
+}
