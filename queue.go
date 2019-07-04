823d65969a9db61ed4c3df70310e4e87f5baae46
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-07-04 10:51:10 +0200

    added queue type

--- queue.go
+++ queue.go
@@ -5,49 +5,39 @@ import (
 	"time"
 )
 
+// QueueConfig is used to configure a queue.
 type QueueConfig struct {
+	// The prefix for all keys.
 	Prefix string
 
-	Skip      int
-	Batch     int
-	Window    int
-	Cache     int
-	Retention int
-	Limit     int
+	// The amount of entries to cache in memory.
+	Cache int
 
-	Timeout  time.Duration
-	Retry    int
-	Delay    time.Duration
+	// The amount of entries to keep around.
+	Retention int
+
+	// The maximum length of the ledger.
+	Limit int
+
+	// The point after which entries are dropped no matter what.
+	Threshold int
+
+	// The interval of periodic cleanings.
 	Interval time.Duration
 
+	// The channel on which cleaning errors are sent.
 	Errors chan<- error
 }
 
-func DynamicConfig(rate int, loss time.Duration) QueueConfig {
-	return QueueConfig{
-		Skip:      100,
-		Batch:     1000,   // * 10
-		Window:    5000,   // * 5
-		Cache:     10000,  // * 2
-		Retention: 10000,  // * 1
-		Limit:     100000, // * 10
-
-		Timeout:  10 * time.Millisecond,
-		Retry:    1000,
-		Delay:    10 * time.Millisecond,
-		Interval: time.Second,
-	}
-}
-
+// Queue is a managed ledger and table with a cleaner.
 type Queue struct {
-	config   QueueConfig
-	ledger   *Ledger
-	table    *Table
-	matrix   *Matrix
-	producer *Producer
-	cleaner  *Cleaner
+	config  QueueConfig
+	ledger  *Ledger
+	table   *Table
+	cleaner *Cleaner
 }
 
+// CreateQueue will create and return a new queue based on the provided settings.
 func CreateQueue(db *DB, config QueueConfig) (*Queue, error) {
 	// create ledger
 	ledger, err := CreateLedger(db, LedgerConfig{
@@ -67,77 +57,44 @@ func CreateQueue(db *DB, config QueueConfig) (*Queue, error) {
 		return nil, err
 	}
 
-	// create matrix
-	matrix, err := CreateMatrix(db, MatrixConfig{
-		Prefix: fmt.Sprintf("%s:m", config.Prefix),
-	})
-	if err != nil {
-		return nil, err
-	}
-
-	// create producer
-	producer := NewProducer(ledger, ProducerConfig{
-		Batch:   config.Batch,
-		Timeout: config.Timeout,
-		Retry:   config.Retry,
-		Delay:   config.Delay,
-	})
-
 	// create cleaner
 	cleaner := NewCleaner(ledger, CleanerConfig{
 		Retention: config.Retention,
-		Threshold: 0, // disable
+		Threshold: config.Threshold,
 		Interval:  config.Interval,
 		Tables:    []*Table{table},
-		Matrices:  []*Matrix{matrix},
 		Errors:    config.Errors,
 	})
 
 	return &Queue{
-		config:   config,
-		ledger:   ledger,
-		table:    table,
-		matrix:   matrix,
-		producer: producer,
-		cleaner:  cleaner,
+		config:  config,
+		ledger:  ledger,
+		table:   table,
+		cleaner: cleaner,
 	}, nil
 }
 
-func (q *Queue) Write(entry Entry, ack func(error)) bool {
-	return q.producer.Write(entry, ack)
+// Ledger will return the queues ledger.
+func (q *Queue) Ledger() *Ledger {
+	return q.ledger
 }
 
-func (q *Queue) Reader(start uint64, entries chan<- Entry, errors chan<- error) *Reader {
-	return NewReader(q.ledger, ReaderConfig{
-		Start:   start,
-		Entries: entries,
-		Errors:  errors,
-		Batch:   q.config.Batch,
-	})
+// Table will return the queues table.
+func (q *Queue) Table() *Table {
+	return q.table
 }
 
-func (q *Queue) Consumer(name string, entries chan<- Entry, errors chan<- error) *Consumer {
-	return NewConsumer(q.ledger, q.table, ConsumerConfig{
-		Name:    name,
-		Entries: entries,
-		Errors:  errors,
-		Batch:   q.config.Batch,
-		Skip:    q.config.Skip,
-	})
+// Producer will create a new producer with the provided config.
+func (q *Queue) Producer(config ProducerConfig) *Producer {
+	return NewProducer(q.ledger, config)
 }
 
-func (q *Queue) Worker(name string, entries chan<- Entry, errors chan<- error) *Worker {
-	return NewWorker(q.ledger, q.matrix, WorkerConfig{
-		Name:    name,
-		Entries: entries,
-		Errors:  errors,
-		Batch:   q.config.Batch,
-		Window:  q.config.Window,
-		Skip:    q.config.Skip,
-	})
+// Consumer will create a new consumer with the provided config.
+func (q *Queue) Consumer(config ConsumerConfig) *Consumer {
+	return NewConsumer(q.ledger, q.table, config)
 }
 
+// Close will close the queue.
 func (q *Queue) Close() {
-	// close cleaner
 	q.cleaner.Close()
 }
---
+++ queue_test.go
@@ -0,0 +1,102 @@
+package quasar
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestQueue(t *testing.T) {
+	db := openDB(true)
+
+	errors := make(chan error, 100)
+	go func() {
+		for err := range errors {
+			if err != nil {
+				panic(err)
+			}
+		}
+	}()
+
+	queue, err := CreateQueue(db, QueueConfig{
+		Prefix:    "queue",
+		Cache:     10,
+		Retention: 20,
+		Limit:     30,
+		Interval:  5 * time.Millisecond,
+		Errors:    errors,
+	})
+	assert.NoError(t, err)
+
+	entries := make(chan Entry, 1)
+
+	consumer := queue.Consumer(ConsumerConfig{
+		Name:    "foo",
+		Entries: entries,
+		Errors:  errors,
+		Batch:   10,
+		Window:  10,
+	})
+
+	time.Sleep(50 * time.Millisecond)
+
+	producer := queue.Producer(ProducerConfig{
+		Batch:   5,
+		Timeout: time.Millisecond,
+		Retry:   100,
+		Delay:   10 * time.Millisecond,
+	})
+
+	done := make(chan struct{})
+
+	go func(){
+		for i := 1; i <= 50; i++ {
+			ok := producer.Write(Entry{Sequence: uint64(i), Payload: []byte("foo")}, func(err error) {
+				errors <- err
+			})
+			assert.True(t, ok)
+		}
+
+		close(done)
+	}()
+
+	i := 1
+	for {
+		entry := <-entries
+		assert.Equal(t, uint64(i), entry.Sequence)
+
+		consumer.Mark(entry.Sequence, true, func(err error) {
+			errors <- err
+		})
+
+		if i == 25 {
+			break
+		}
+
+		i++
+	}
+
+	<-done
+	time.Sleep(50 * time.Millisecond)
+
+	producer.Close()
+	consumer.Close()
+
+	time.Sleep(50 * time.Millisecond)
+
+	length := queue.Ledger().Length()
+	assert.Equal(t, 25, length)
+
+	head := queue.Ledger().Head()
+	assert.Equal(t, 50, int(head))
+
+	low, high, _, err := queue.Table().Range()
+	assert.NoError(t, err)
+	assert.Equal(t, 25, int(low))
+	assert.Equal(t, 25, int(high))
+
+	queue.Close()
+
+	close(errors)
+}
