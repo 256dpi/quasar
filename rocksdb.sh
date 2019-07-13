56e790455da263e7f0118ee7c337482cebd941d1
Joël Gähwiler <joel.gaehwiler@gmail.com>
2019-07-13 20:47:38 +0200

    try to use prebuilt libs

--- rocksdb.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt-get update -qq
-sudo apt-get install libsnappy-dev zlib1g-dev libbz2-dev -qq
-
-git clone https://github.com/facebook/rocksdb.git rocksdb
-cd rocksdb
-git reset --hard v5.18.3
-make shared_lib
